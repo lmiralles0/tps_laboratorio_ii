34efa656d793f4e4776dbcf4c6fe61b85f52d840
lmiralles0 <60629914+lmiralles0@users.noreply.github.com>
2022-06-23 15:32:43 -0300

    Add files via upload

---
+++ RecuperatoriosTP/TP1/LucianoMiralles.2C.TP1/Entidades/Calculadora.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    internal static class Calculadora
+    {
+        private static char ValidarOperador(char operador)
+        {
+            if (operador != '+' && operador != '-' && operador != '/' && operador != '*')
+                return '+';
+            else
+                return operador;
+        }
+
+        public static double Operar(Operando num1, Operando num2, char operador)
+        {
+            
+     
+        }
+    }
+}
---
+++ RecuperatoriosTP/TP1/LucianoMiralles.2C.TP1/Entidades/Operando.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Operando
+    {
+        private double numero;
+        public Operando()
+        {
+            numero = 0;
+        }
+
+        public Operando(double numero) : this()
+        {
+            this.numero = numero;
+        }
+
+        public Operando(string strString)
+        {
+            Numero = strString;
+        }
+
+        private double ValidarOperando(string StrNumero)
+        {
+            if (string.IsNullOrEmpty(StrNumero))
+                return Convert.ToDouble(StrNumero);
+            return 0;
+        }
+
+        private string Numero 
+        {
+            set
+            { 
+                this.numero = ValidarOperando(value); 
+            } 
+        }
+
+        private bool EsBinario(string binario)
+        {
+            bool respuesta = false;
+            if (!(string.IsNullOrEmpty(binario)))
+            {
+                foreach(char c in binario)
+                {
+                    if (c != '0' && c !='1')
+                    {
+                        respuesta = false;
+                        break;
+                    }
+                    respuesta = true;
+                }
+            }
+            return respuesta;
+        }
+
+
+
+
+
+        public static double operator +(Operando n1, Operando n2)
+        {
+            return n1.numero + n2.numero;
+        }
+
+        public static double operator -(Operando n1, Operando n2)
+        {
+            return n1.numero - n2.numero;
+        }
+
+        public static double operator *(Operando n1, Operando n2)
+        {
+            return n1.numero * n2.numero;
+        }
+
+        public static double operator /(Operando n1, Operando n2)
+        {
+            if (n2.numero != 0)
+                return n1.numero / n2.numero;
+            return 0;
+        }
+
+
+
+
+    }
+}
