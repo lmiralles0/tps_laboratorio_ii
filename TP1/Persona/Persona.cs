61993545c239d9b0d1f4b4a988bcec8c45871f5a
lmiralles0 <60629914+lmiralles0@users.noreply.github.com>
2022-09-26 17:36:30 -0300

    Add files via upload

---
+++ TP1/Persona/Persona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace Persona
+{
+    internal abstract class Persona
+    {
+        public int dni;
+        public string nombre;
+        public string apellido;
+
+        internal Persona()
+        {
+            dni = 0;
+            nombre = "";
+            apellido = "";
+        }
+    }
+}
