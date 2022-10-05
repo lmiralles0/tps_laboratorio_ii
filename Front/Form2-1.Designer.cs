3f33bfdc5cbe0c629a138be91d3e2ec4c4afee06
lmiralles0 <60629914+lmiralles0@users.noreply.github.com>
2022-10-05 18:32:04 -0300

    Add files via upload

---
+++ Front/Form2-1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Front
+{
+    partial class Form2_1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxAlta = new System.Windows.Forms.GroupBox();
+            this.groupBoxAlta2 = new System.Windows.Forms.GroupBox();
+            this.linkLabelRa = new System.Windows.Forms.LinkLabel();
+            this.groupBoxAlta2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxAlta
+            // 
+            this.groupBoxAlta.Location = new System.Drawing.Point(467, 82);
+            this.groupBoxAlta.Margin = new System.Windows.Forms.Padding(0, 3, 3, 3);
+            this.groupBoxAlta.Name = "groupBoxAlta";
+            this.groupBoxAlta.Size = new System.Drawing.Size(250, 299);
+            this.groupBoxAlta.TabIndex = 20;
+            this.groupBoxAlta.TabStop = false;
+            this.groupBoxAlta.Visible = false;
+            // 
+            // groupBoxAlta2
+            // 
+            this.groupBoxAlta2.Controls.Add(this.linkLabelRa);
+            this.groupBoxAlta2.Location = new System.Drawing.Point(145, 143);
+            this.groupBoxAlta2.Name = "groupBoxAlta2";
+            this.groupBoxAlta2.Size = new System.Drawing.Size(220, 100);
+            this.groupBoxAlta2.TabIndex = 22;
+            this.groupBoxAlta2.TabStop = false;
+            this.groupBoxAlta2.Visible = false;
+            // 
+            // linkLabelRa
+            // 
+            this.linkLabelRa.AutoSize = true;
+            this.linkLabelRa.Location = new System.Drawing.Point(6, 22);
+            this.linkLabelRa.Name = "linkLabelRa";
+            this.linkLabelRa.Size = new System.Drawing.Size(119, 15);
+            this.linkLabelRa.TabIndex = 21;
+            this.linkLabelRa.TabStop = true;
+            this.linkLabelRa.Text = "Regularidad alumnos";
+            // 
+            // Form2_1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBoxAlta2);
+            this.Controls.Add(this.groupBoxAlta);
+            this.Name = "Form2_1";
+            this.Text = "Form2_1";
+            this.groupBoxAlta2.ResumeLayout(false);
+            this.groupBoxAlta2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.GroupBox groupBoxAlta;
+        public System.Windows.Forms.GroupBox groupBoxAlta2;
+        private System.Windows.Forms.LinkLabel linkLabelRa;
+    }
+}
---
+++ Front/Form2.cs
@@ -0,0 +1,102 @@
+﻿using Persona;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Menu;
+
+namespace Front
+{
+    public partial class Form2 : Form
+    {
+ 
+        public Form2()
+        {
+            InitializeComponent();
+
+        }
+
+        private void toolStripMenuItem1_MouseHover(object sender, EventArgs e)
+        {
+            this.toolStripMenuItem1.ShowDropDown() ;
+        }
+        private void linkLabelAu_LinkClicked_1(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.groupBoxAlta2.Visible = true;
+            this.groupBoxAlta.Visible = true;
+            this.labelUser.Visible = true;
+            this.labelLastName.Visible = true;
+            this.labelName.Visible = true;
+            this.labelDni.Visible = true;
+            this.labelPasswd.Visible = true;
+            this.AltaUserComboBox.Visible = true;
+            this.AltaTextBoxUserSureName.Visible = true;
+            this.AltaTextBoxUserName.Visible = true;
+            this.AltaTextBoxUserDni.Visible = true;
+            this.AltaTextBoxUserPasswd.Visible = true;
+            this.linkLabelAu.Visible = false;
+            this.linkLabelRa.Visible = false;
+            this.linkLabelAm.Visible = false;
+            this.linkLabelAp.Visible = false;
+            this.linkLabelAu2.Visible = true;
+            this.buttonCerrarAlta.Visible = true;
+            this.buttonAgregarAlta.Visible = true;
+        }
+
+        private void buttonCerrarAlta_Click(object sender, EventArgs e)
+        {
+            this.groupBoxAlta2.Visible = false;
+            this.groupBoxAlta.Visible = false;
+            this.labelUser.Visible = false;
+            this.labelLastName.Visible = false;
+            this.labelName.Visible = false;
+            this.labelDni.Visible = false;
+            this.labelPasswd.Visible = false;
+            this.AltaUserComboBox.Visible = false;
+            this.AltaTextBoxUserSureName.Visible = false;
+            this.AltaTextBoxUserName.Visible = false;
+            this.AltaTextBoxUserDni.Visible = false;
+            this.AltaTextBoxUserPasswd.Visible = false;
+            this.linkLabelAu.Visible = true;
+            this.linkLabelRa.Visible = true;
+            this.linkLabelAm.Visible = true;
+            this.linkLabelAp.Visible = true;
+            this.buttonCerrarAlta.Visible = false;
+            this.buttonAgregarAlta.Visible = false;
+
+        }
+
+        private void linkLabelRa_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.groupBoxLinkLabelRa.Visible = true;
+            this.checkedListBox1.Visible = true;
+            this.buttonCerrarRa.Visible = true;
+            this.groupBoxRa.Visible = true;
+            this.linkLabelRa2.Visible = true;
+            this.linkLabelRa.Visible = false;
+            this.linkLabelAp.Visible = false;
+            this.linkLabelAm.Visible = false;
+            this.linkLabelAu.Visible = false;
+            
+        }
+
+        private void buttonCerrarRa_Click(object sender, EventArgs e)
+        {
+            this.groupBoxLinkLabelRa.Visible = false;
+            this.groupBoxRa.Visible = false;
+            this.linkLabelRa2.Visible = false;
+            this.linkLabelRa.Visible = true;
+            this.checkedListBox1.Visible = false;
+            this.buttonCerrarRa.Visible = false;
+            this.linkLabelAp.Visible = true;
+            this.linkLabelAm.Visible = true;
+            this.linkLabelAu.Visible = true;
+            
+        }
+    }
+}
